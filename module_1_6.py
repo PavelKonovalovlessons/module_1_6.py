b1269b05b8a0233a46cf4cae07e246683e2f6bf4
PavelKonovalovlessons <konovnovv@mail.ru>
2024-07-31 16:15:14 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,16 @@
+my_dict = {'Ivan': 2003, 'Dmitri': 1999}
+my_dict['Emelya'] = 1998
+my_dict['Oleg'] = 2000
+del my_dict ['Dmitri']
+print(my_dict)
+
+print(my_dict['Ivan'])
+print(my_dict)
+
+
+my_set = {1, 2 , 3, 'string', 16, 3, 2 , 1 , 'string'}
+print(my_set)
+print(my_set.add(12))
+print(my_set.add(222))
+print(my_set.remove(1))
+print(my_set)
